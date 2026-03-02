513bd665f93e32192fbec6551d92736faebe299e
gtpkyr-byte <gtpkyr@gmail.com>
2026-03-01 20:56:09 -0600

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Disable Turbopack to use stable Webpack bundler which is fully compatible with Tailwind v3
-  experimental: {},
 }
 
 export default nextConfig
